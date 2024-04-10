e6eb7a743c1248ee77b90f5db9ca0f3502551cbd
Carolina Maldonado Torres <cmaldonado@limitronic.com>
2024-04-10 16:06:15 +0200

    Two-way databinding

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
@@ -12,11 +13,47 @@ export class ProductCardComponent {
     image: 'https://images-eu.ssl-images-amazon.com/images/I/617Zkh+WY8S._SLDPMOBCAROUSELAUTOCROP288221_MCnd_AC_SR462,693_.jpg',
     name: 'Awesome Metal Salad',
     description: 'The Football Is Good For Training And Recreational Purposes',
-    unitAvailable: 0,
+    unitAvailable: 10,
   }
 
   productImageStyle = {
     border: '1px solid black',
-    filter: 'grayscale(100%)',
+    filter: 'grayscale(0)',
+  }
+
+  formState = false //manejamos el estado en .html
+  address = ''
+  postalCode = 0
+
+
+  orderForm = new FormGroup({
+    address: new FormControl(''),
+    postalCode: new FormControl(''),
+
+})
+
+  buyProduct() {
+    console.log('Comprar este producto');
+    this.formState = true
+  }
+  
+  orderProduct() {
+  const orderData = this.orderForm.value;
+  console.log(orderData);
+}
+
+
+  addToCart() {
+    console.log('Agregamos un producto al carrito');
+  }
+
+  onMouseEnter() {
+    console.log('Mouse enter');
+    this.productImageStyle = {...this.productImageStyle, filter: 'grayscale(100%)'}
+}
+
+  onMouseLeave() {
+    console.log('Mouse leave');
+    this.productImageStyle = {...this.productImageStyle, filter: 'grayscale(0)'}
   }
 }
