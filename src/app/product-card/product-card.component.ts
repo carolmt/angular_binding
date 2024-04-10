8fb0d4e8ff61fff6868335d8b48da9f25d6d3c78
Carolina Maldonado Torres <cmaldonado@limitronic.com>
2024-04-10 14:09:25 +0200

    corregidas las condiciones .html

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -9,9 +9,14 @@ import { Component } from '@angular/core';
 })
 export class ProductCardComponent {
   product = {
-    image: 'https://github.com/carolmt.png',
+    image: 'https://images-eu.ssl-images-amazon.com/images/I/617Zkh+WY8S._SLDPMOBCAROUSELAUTOCROP288221_MCnd_AC_SR462,693_.jpg',
     name: 'Awesome Metal Salad',
     description: 'The Football Is Good For Training And Recreational Purposes',
     unitAvailable: 0,
+  }
+
+  productImageStyle = {
+    border: '1px solid black',
+    filter: 'grayscale(100%)',
   }
 }
