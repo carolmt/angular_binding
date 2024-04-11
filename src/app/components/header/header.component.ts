e5034c67c1f5f67ae6dfe8875152e1e16da32d72
Carolina Maldonado Torres <carolinamaldonado12@hotmail.es>
2024-04-11 16:32:21 +0200

    done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component} from '@angular/core';
 import { TitleComponent } from '../title/title.component';
 import { AvatarComponent } from '../avatar/avatar.component';
+import { RouterLinkActive, RouterLink} from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TitleComponent, AvatarComponent],
+  imports: [TitleComponent, AvatarComponent, RouterLinkActive, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
